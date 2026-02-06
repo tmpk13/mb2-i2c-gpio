8b985c4b6d5631667af9473bf919842539339bfe
tmpk13 <>
2026-02-06 14:22:33 -0800

    Pin to binary converter added

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;                                                    
 use embedded_hal::i2c::{I2c, Error};
 
+// Pin
+// 5   :   0000 100
+// Hex     0x16
+
 
 const ADDR: u8 = 0x20;
 pub struct GpioExpander<I2C> {
@@ -33,6 +37,16 @@ impl<I2C: I2c> GpioExpander<I2C> {
         self.i2c.write_read(ADDR, &[0x00], &mut temp)?;
         Ok(temp[0])
     }
+    pub fn gpio_write(&mut self, value: u8) -> Result<u8, I2C::Error> {
+        let mut temp = [0];
+        self.i2c.write_read(ADDR, &[value], &mut temp)?;
+        Ok(temp[0])
+    }
+
+    pub fn pin_to_hex(&self, pin: u32) -> u32 {
+        // 5 -> 16
+        2u32.pow(pin-1).clone()
+    }
 }
 
 enum State {
@@ -50,20 +64,21 @@ fn init() -> ! {
     let i2c = twim::Twim::new(board.TWIM0, board.i2c_external.into(), twim::Frequency::K100);
     let mut gpio = GpioExpander::new(i2c);
 
-    board.display_pins.col1.set_low().unwrap();
 
     let mut state = State::LedOff;
+
+
+    assert_eq!(gpio.pin_to_hex(5), 0b001_0000);
+
 
     loop {
         state = match state {
             State::LedOff => {
-                board.display_pins.row1.set_high().unwrap();
                 gpio.gpio_high();
                 rprintln!("high");
                 State::LedOn
             }
             State::LedOn => {
-                board.display_pins.row1.set_low().unwrap();
                 gpio.gpio_low();
                 rprintln!("low");
                 State::LedOff
