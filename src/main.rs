2091583cf24a4daa98a97c6edaad8b27031e0e0f
tmpk13 <>
2026-02-06 15:13:33 -0800

    Pin to binary converter multiple pins

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 #![no_main]
 #![no_std]
 
-
 // https://github.com/pdx-cs-rust-embedded/blinky-rs/
 use cortex_m_rt::entry;
+use embedded_hal::i2c::{Error, I2c};
 use embedded_hal::{delay::DelayNs, digital::OutputPin, i2c};
 use microbit::{
     board::{Board, I2CExternalPins},
     hal::{timer::Timer, twim},
 };
-use rtt_target::{rtt_init_print, rprintln};                                   
-use panic_rtt_target as _;                                                    
-use embedded_hal::i2c::{I2c, Error};
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 // 0x03
 const H: u8 = 0x42;
@@ -36,20 +35,17 @@ impl<I2C: I2c> GpioExpander<I2C> {
         Self { i2c }
     }
 
-    pub fn gpio_high(&mut self) -> Result<u8, I2C::Error> {
-        let mut temp = [0];
-        self.i2c.write_read(ADDR, &[0xFF], &mut temp)?;
-        Ok(temp[0])
+    pub fn low(&mut self) -> Result<(), I2C::Error> {
+        self.i2c.write(ADDR, &[0x00])?;
+        Ok(())
     }
-    pub fn gpio_low(&mut self) -> Result<u8, I2C::Error> {
-        let mut temp = [0];
-        self.i2c.write_read(ADDR, &[0x00], &mut temp)?;
-        Ok(temp[0])
+    pub fn high(&mut self) -> Result<(), I2C::Error> {
+        self.i2c.write(ADDR, &[0xFF])?;
+        Ok(())
     }
-    pub fn gpio_write(&mut self, value: u8) -> Result<u8, I2C::Error> {
-        let mut temp = [0];
-        self.i2c.write_read(ADDR, &[value], &mut temp)?;
-        Ok(temp[0])
+    pub fn write(&mut self, value: u8) -> Result<(), I2C::Error> {
+        self.i2c.write(ADDR, &[value])?;
+        Ok(())
     }
 
     pub fn pin_to_hex(&self, pin: u8) -> u8 {
@@ -59,10 +55,13 @@ impl<I2C: I2c> GpioExpander<I2C> {
     pub fn pins_to_hex(&self, pins: &[u8]) -> u8 {
         let mut value: u8 = 0x00;
         // Add up pins
-        for pin in 0usize..8usize { 
-            value += 2u8.pow((pins[pin] as u32) - 1);
-        }
-        // value = pins.iter().map(|x| value + x);
+        // for pin in 0usize..8usize {
+        //     value += 2u8.pow((pins[pin] as u32) - 1);
+        // }
+        value = pins.iter().fold(
+            0, 
+            |sum, pin| sum + 2u8.pow((*pin-1) as u32)
+        );
         value
     }
 }
@@ -79,25 +78,26 @@ fn init() -> ! {
     let mut timer = Timer::new(board.TIMER0);
 
     //board.i2c_external
-    let i2c = twim::Twim::new(board.TWIM0, board.i2c_external.into(), twim::Frequency::K100);
+    let i2c = twim::Twim::new(
+        board.TWIM0,
+        board.i2c_external.into(),
+        twim::Frequency::K100,
+    );
     let mut gpio = GpioExpander::new(i2c);
-
 
     let mut state = State::LedOff;
 
-
     assert_eq!(gpio.pin_to_hex(5), 0b0001_0000);
-
 
     loop {
         state = match state {
             State::LedOff => {
-                gpio.gpio_high();
+                gpio.high();
                 rprintln!("high");
                 State::LedOn
             }
             State::LedOn => {
-                gpio.gpio_low();
+                gpio.low();
                 rprintln!("low");
                 State::LedOff
             }
@@ -105,5 +105,3 @@ fn init() -> ! {
         timer.delay_ms(500);
     }
 }
-
-
